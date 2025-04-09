7a5a7671d8c7315fffc663cfe07b22ae5c7ac282
Harinandan97 <hariharinandan1@gmail.com>
2025-04-09 11:43:53 +0530

    Delete lib directory

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:flutter/cupertino.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-
-import 'machine test/view/userhome.dart';
-
-void main() {
-  runApp(GetMaterialApp(home: ProductHome()));
-}
