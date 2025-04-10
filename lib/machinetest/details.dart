67c67b07f6a06b61a57645773a6c7f665cbfcda2
harinandan97 <hariharinandan1@gmail.com>
2025-04-10 11:19:24 +0530

    commit

--- lib/machinetest/details.dart
+++ lib/machinetest/details.dart
@@ -34,30 +34,4 @@ class UserDetailScreen extends StatelessWidget {
   }
 }
 
-class InfoRow extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final String value;
-
-  const InfoRow({
-    super.key,
-    required this.icon,
-    required this.label,
-    required this.value,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        children: [
-          Icon(icon, color: Colors.blueAccent),
-          SizedBox(width: 12),
-          Text("$label: ", style: TextStyle(fontWeight: FontWeight.bold)),
-          Expanded(child: Text(value, overflow: TextOverflow.ellipsis)),
-        ],
-      ),
-    );
-  }
-}
+//
